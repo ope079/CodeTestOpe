ad18f263a98e0a7f0fc1531808e3f7f2bc8b4834
ope079 <o_orekoya@yahoo.com>
2021-05-02 22:05:47 +0100

    updated scan for duplicates

--- application/headr_and_consu_methods.py
+++ application/headr_and_consu_methods.py
@@ -5,7 +5,6 @@ from application import db
 from sqlalchemy import func
 
 def validate_meter_number_and_date_time(meter_number, measurement_date_time):
-    if Consu.query.filter(and_(Consu.meter_number==meter_number, Consu.measurement_date_time==measurement_date_time)).count() > 1:
+    if Consu.query.filter(and_(Consu.meter_number==meter_number, Consu.measurement_date_time==measurement_date_time)).distinct().count() > 1:
         subq = db.session.query(Consu.measurement_date_time).filter(and_(Consu.meter_number==meter_number, Consu.measurement_date_time==measurement_date_time)).order_by(Consu.measurement_date_time.desc()).limit(1).subquery()
-        Consu.query.filter(Consu.measurement_date_time.notin_(subq)).delete(synchronize_session=False)
-        db.session.execute("ALTER SEQUENCE Consu.id RESTART WITH 1")
+        Consu.query.filter(Consu.meter_number==meter_number and Consu.measurement_date_time and Consu.measurement_date_time.notin_(subq)).delete(synchronize_session=False)
